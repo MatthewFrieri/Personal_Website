b19b7dd7d8577ead25ab4f59889b23a84fec66c4
matfr <mat.frieri@gmail.com>
2024-05-15 14:46:46 -0400

    finished home screen

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ function setActive(id) {
 
 // Parallax Scrolling
 
-const myName = document.getElementById('name');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const slogan1 = document.getElementById('one');
+const slogan2 = document.getElementById('two');
+const slogan3 = document.getElementById('three');
+
 const trees_1 = document.getElementById('parallax_1');
 const hill_2 = document.getElementById('parallax_2');
 const fog_3 = document.getElementById('parallax_3');
@@ -27,15 +32,20 @@ const sky_9 = document.getElementById('parallax_9');
 window.addEventListener('scroll', () => {
    const scrollY = window.scrollY;
 
-   myName.style.marginTop  = scrollY * 0.8 + 'px';
-   sky_9.style.marginTop   = scrollY * 0.5 + 'px';
-   mtn_8.style.marginTop   = scrollY * 0.5 + 'px';
-   cloud_7.style.marginTop = scrollY * 0.4 + 'px';
-   mtn_6.style.marginTop   = scrollY * 0.4 + 'px';
-   fog_5.style.marginTop   = scrollY * 1.2 + 'px';
-   mtn_4.style.marginTop   = scrollY * 0.3 + 'px';
-   fog_3.style.marginTop   = scrollY * 0.4 + 'px';
-   hill_2.style.marginTop  = scrollY * 0.1 + 'px';
-   trees_1.style.marginTop = scrollY * 0   + 'px';
+   firstName.style.marginTop = scrollY * 0.8 + 'px';
+   lastName.style.marginTop  = scrollY * 0.8 + 'px';
+   slogan1.style.marginTop    = scrollY * 2  + 'px';
+   slogan2.style.marginTop    = scrollY * 2  + 'px';
+   slogan3.style.marginTop    = scrollY * 2  + 'px';
+
+   sky_9.style.marginTop     = scrollY * 0.5 + 'px';
+   mtn_8.style.marginTop     = scrollY * 0.5 + 'px';
+   cloud_7.style.marginTop   = scrollY * 0.4 + 'px';
+   mtn_6.style.marginTop     = scrollY * 0.4 + 'px';
+   fog_5.style.marginTop     = scrollY * 1.2 + 'px';
+   mtn_4.style.marginTop     = scrollY * 0.3 + 'px';
+   fog_3.style.marginTop     = scrollY * 0.4 + 'px';
+   hill_2.style.marginTop    = scrollY * 0.1 + 'px';
+   trees_1.style.marginTop   = scrollY * 0   + 'px';
 
 });
