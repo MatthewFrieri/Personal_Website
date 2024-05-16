2db34c92d94534119ceda84ce186f35fe44fdb7f
matfr <mat.frieri@gmail.com>
2024-05-15 23:57:25 -0400

    about section and jump down arrow on home page

--- app.js
+++ app.js
@@ -11,32 +11,39 @@ function setActive(id) {
     toBeActive.classList.add('active');
 }
 
+
 // Parallax Scrolling
 
 const firstName = document.getElementById('first-name');
 const lastName = document.getElementById('last-name');
+
 const slogan1 = document.getElementById('one');
 const slogan2 = document.getElementById('two');
 const slogan3 = document.getElementById('three');
 
-const trees_1 = document.getElementById('parallax_1');
-const hill_2 = document.getElementById('parallax_2');
-const fog_3 = document.getElementById('parallax_3');
-const mtn_4 = document.getElementById('parallax_4');
-const fog_5 = document.getElementById('parallax_5');
-const mtn_6 = document.getElementById('parallax_6');
-const cloud_7 = document.getElementById('parallax_7');
-const mtn_8 = document.getElementById('parallax_8');
-const sky_9 = document.getElementById('parallax_9');
+const downArrow = document.getElementById('down-arrow');
+
+const trees_1 = document.getElementById('parallax-1');
+const hill_2 = document.getElementById('parallax-2');
+const fog_3 = document.getElementById('parallax-3');
+const mtn_4 = document.getElementById('parallax-4');
+const fog_5 = document.getElementById('parallax-5');
+const mtn_6 = document.getElementById('parallax-6');
+const cloud_7 = document.getElementById('parallax-7');
+const mtn_8 = document.getElementById('parallax-8');
+const sky_9 = document.getElementById('parallax-9');
 
 window.addEventListener('scroll', () => {
    const scrollY = window.scrollY;
 
    firstName.style.marginTop = scrollY * 0.8 + 'px';
    lastName.style.marginTop  = scrollY * 0.8 + 'px';
-   slogan1.style.marginTop    = scrollY * 2  + 'px';
-   slogan2.style.marginTop    = scrollY * 2  + 'px';
-   slogan3.style.marginTop    = scrollY * 2  + 'px';
+   
+   slogan1.style.marginTop   = scrollY * 2   + 'px';
+   slogan2.style.marginTop   = scrollY * 2   + 'px';
+   slogan3.style.marginTop   = scrollY * 2   + 'px';
+
+   downArrow.style.marginTop = scrollY * 2   + 'px'; 
 
    sky_9.style.marginTop     = scrollY * 0.5 + 'px';
    mtn_8.style.marginTop     = scrollY * 0.5 + 'px';
